0b4e55dadb13145cdeaa1c82dd975e7129411bc2
Corsenac Pierre <pierre.corsenac@epitech.eu>
2022-03-24 19:08:59 +0100

    Setting up project and Labels/TextField on the ProfileViewController

---
+++ TestKlaxit/CustomTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTableViewCell.swift
+//  TestKlaxit
+//
+//  Created by Pierre Corsenac on 24/03/2022.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TestKlaxit/ProfileViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ProfileViewController.swift
+//  TestKlaxit
+//
+//  Created by Pierre Corsenac on 24/03/2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var phoneNumber: UITextField!
+    @IBOutlet weak var corporationName: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var jobName: UITextField!
+    @IBOutlet weak var homeAddress: UITextField!
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
